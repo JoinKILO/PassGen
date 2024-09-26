58ae20eecb0946b172c058d5e2a2ba4ea92089d1
JoinKILO <gamerstaff3290@gmail.com>
2024-09-26 11:32:16 +0300

    Добавленны пременные для настройки

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from PIL import Image
 
 # Инициализация переменных
 can_resizable = False
+theme = "themes/violet.json"
+appearance = "system"
 
 # Настройки главного окна
 root = CTk()
@@ -11,26 +13,27 @@ root.geometry("1400x920")
 root.title("PASSGEN - Password generator")
 root.resizable(width=False, height=False)
 
-# Временная тема
-set_default_color_theme("blue")
+set_appearance_mode(appearance)
+set_default_color_theme(theme)
 
-# Возможно стоит возвести всё в класс Option В котором будут храниться настройки приложения
+
 # Когда свитч переключается
 def on_resize_option_changed():
     if resize_option.get() == 0: # Если свитч выключен запретить изменение окна
         can_resizable = False
-        root.resizable(width=can_resizable, height=can_resizable)
     elif resize_option.get() == 1: # Если свитч включен разрешить изменение окна
         can_resizable = True
-        root.resizable(width=can_resizable, height=can_resizable)
+    root.resizable(width=can_resizable, height=can_resizable)
+
 
 # Создание табов и расположение их справа
 tabs = CTkTabview(master=root, width=400, height=900, anchor="w")
-tabs.add("Option")
+tabs.add("Настройки")
+tabs.add("Темы  ")
 
-# Создаем свитч в Option табе
-resize_option = CTkSwitch(tabs.tab("Option"), text="Разрешить изменение окна", 
-                          command=on_resize_option_changed)
+# Создаем свитч в табе настроек
+resize_option = CTkSwitch(tabs.tab("Настройки"), text="Разрешить изменение окна", 
+                            command=on_resize_option_changed)
 
 # Расположение экземпляров
 resize_option.pack(anchor="n")
