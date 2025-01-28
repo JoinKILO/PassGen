ede355da151837606f08deb3dd11e0c3fcf4c4da
JoinKILO <gamerstaff3290@gmail.com>
2025-01-28 10:37:12 +0300

    Add lang.csv (russian, english)

--- app/set_locale.py
+++ app/set_locale.py
@@ -1,13 +1,5 @@
-import csv
 import os
 import sys
 
 class Locale():
-    language = "app\data\lang.csv"
-    
-    def get_localization(self) -> None:
-        with open(self.language, 'r') as lang:
-            file = csv.DictReader(self.language)
-            
-        return file
-print('class passed')
+    pass
